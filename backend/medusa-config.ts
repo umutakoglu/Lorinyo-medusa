253e5423fb6c76b06b3ecd7043e08583825d7be4
UmuT <32216538+umutakoglu@users.noreply.github.com>
2026-01-04 02:15:03 +0300

    Initial commit

---
+++ backend/medusa-config.ts
@@ -0,0 +1,16 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS || "http://localhost:8000",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:5173,http://localhost:9000",
+      authCors: process.env.AUTH_CORS || "http://localhost:5173,http://localhost:9000",
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  }
+})
